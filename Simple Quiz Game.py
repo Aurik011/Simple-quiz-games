fc53bd241de68defebab0580e698dfde4616932b
Aurik Anjum <109861996+Aurik011@users.noreply.github.com>
2022-10-21 16:33:47 +0600

    SECOND

--- Simple Quiz Game.py
+++ Simple Quiz Game.py
@@ -1,12 +1,13 @@
+# SIMPLE QUIZ GAME
 print("Welcome to the quiz game")
 
-willPlay = input("Will you play? ")
+willPlay = input("Will you play? ") # prompt
 
 if willPlay.lower() != "yes": # so that Yes or YES results in play mode
     quit()
 
 print("Entering test play mode:")
-score = 0
+score = 0 # INITIALLY SCORE COUNTER IS ZERO
 
 answer = input("Who is the founder of Microsoft? ")
 if answer.lower() == "bill gates": # Don't write Bill Gates
@@ -39,3 +40,33 @@ else:
 print("You got " + str(score) + " questions correct")
 print("you got " + str((score/4)*100) + "% questions correct") # percentage of correct answers
 
+
+
+
+# answer = input("Who is the founder of Microsoft? ")
+# if answer.lower() == "bill gates": # Don't write Bill Gates
+#     print("Correct Answer!")
+#     score += 1
+# else:
+#     print("Not correct!")
+#
+# answer = input("What does PC stand for? ")
+# if answer.lower() == "personal computer":
+#     print("Correct Answer!")
+#     score += 1
+# else:
+#     print("Not correct!")
+#
+# answer = input("What does CPU mean? ")
+# if answer.lower() == "central processing unit":
+#     print("Correct Answer!")
+#     score += 1
+# else:
+#     print("Not correct!")
+#
+# answer = input("Who is Shakib Al Hasan? ")
+# if answer.lower() == "cricketer":
+#     print("Correct Answer!")
+#     score += 1
+# else:
+#     print("Not correct!")
